c40d51e347ceb736fd5540cf2c8ca6bec3896390
Axel Vidoni <vidoni.axel@gmail.com>
2022-01-24 22:31:42 +0100

    ajout du model

---
+++ Civpedia/Models/CivpediaModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Civpedia.Models
+{
+    public class CivpediaModel
+    {
+        private List<Dirigeants> lesCivilisations = ListCivilisationData.ListDirigeants;
+
+        public CivpediaModel()
+        {
+        }
+
+        public List<Dirigeants> getCivilisations(string champRecherche)
+        {
+            return lesCivilisations.Where(x => x.NomDirigeant.Contains(champRecherche, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+        }
+    }
+}
--- Civpedia/Startup.cs
+++ Civpedia/Startup.cs
@@ -1,3 +1,4 @@
+using Civpedia.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace Civpedia
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<CivpediaModel>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
