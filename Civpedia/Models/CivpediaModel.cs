03cd297a9fc4f80d4468c6bc2832a19888a6d5bf
Axel Vidoni <vidoni.axel@gmail.com>
2022-01-29 17:23:40 +0100

    Ajout du tri et de la recherche sur tous les champs

--- Civpedia/Models/CivpediaModel.cs
+++ Civpedia/Models/CivpediaModel.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Civpedia.Models
 {
@@ -13,9 +13,196 @@ namespace Civpedia.Models
         {
         }
 
-        public List<Dirigeants> getCivilisations(string champRecherche)
+        public List<Dirigeants> getCivilisations(string RechercheNomDirigeant, string RechercheTitrePassifDirigeant, string RecherchePassifDirigeant, string RechercheNomEmpire, string RechercheTitrePassifEmpire, string RecherchePassifEmpire, string RechercheNomQuartierEmpire, string RechercheQuartierEmpire, string RechercheNomBatimentEmpire, string RechercheBatimentEmpire, string RechercheNomAmenagementEmpire, string RechercheAmenagementEmpire, TriEntity unTri)
         {
-            return lesCivilisations.Where(x => x.NomDirigeant.Contains(champRecherche, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+
+            List<Dirigeants> lesDirigeants = lesCivilisations.AsQueryable().Where(x => x.NomDirigeant.Contains(RechercheNomDirigeant, StringComparison.CurrentCultureIgnoreCase)).OrderBy("NomDirigeant , NomBatimentEmpire desc" ).Select(x => x).ToList();
+
+            List<Dirigeants> lesDirigeants2 = lesCivilisations.Select(x =>x).ToList();
+
+            if (!String.IsNullOrEmpty(RechercheNomDirigeant))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.NomDirigeant.Contains(RechercheNomDirigeant, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheTitrePassifDirigeant))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.TitrePassifDirigeant.Contains(RechercheTitrePassifDirigeant, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RecherchePassifDirigeant))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.PassifDirigeant.Contains(RecherchePassifDirigeant, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheNomEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.NomEmpire.Contains(RechercheNomEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheTitrePassifEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.TitrePassifEmpire.Contains(RechercheTitrePassifEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RecherchePassifEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.PassifEmpire.Contains(RecherchePassifEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheNomQuartierEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.NomQuartierEmpire.Contains(RechercheNomQuartierEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheQuartierEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.QuartierEmpire.Contains(RechercheQuartierEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheNomBatimentEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.NomBatimentEmpire.Contains(RechercheNomBatimentEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheBatimentEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.BatimentEmpire.Contains(RechercheBatimentEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheNomAmenagementEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.NomAmenagementEmpire.Contains(RechercheNomAmenagementEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+            if (!String.IsNullOrEmpty(RechercheAmenagementEmpire))
+            {
+                lesDirigeants2 = lesDirigeants2.Where(x => x.AmenagementEmpire.Contains(RechercheAmenagementEmpire, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            }
+
+            string order = returnOrder(unTri);
+
+            
+            if (!String.IsNullOrEmpty(order))
+            {
+                 lesDirigeants2 = lesCivilisations.AsQueryable().OrderBy(order).Select(x => x).ToList();
+            }
+            foreach (Dirigeants unDirigeant in lesDirigeants)
+            {
+                Console.WriteLine(unDirigeant.NomDirigeant);
+            }
+
+            foreach (Dirigeants unDirigeant in lesDirigeants2)
+            {
+                Console.WriteLine(unDirigeant.NomDirigeant);
+            }
+            //List<Dirigeants> lesDirigeants = lesCivilisations.Where(x => x.NomDirigeant.Contains(champRecherche, StringComparison.CurrentCultureIgnoreCase)).Select(x => x).ToList();
+            return lesDirigeants2;
         }
+
+        private string returnOrder(TriEntity unTri)
+        {
+            List<string> listOrdering = new List<string>();
+            if (unTri.NameDirigeantOrder == "NameDirigeant")
+            {
+                listOrdering.Add("NomDirigeant");
+            }
+            else if (unTri.NameDirigeantOrder == "NameDirigeantDesc")
+            {
+                listOrdering.Add("NomDirigeant desc");
+            }
+
+            if (unTri.TitlePassifDirigeantOrder == "TitlePassifDirigeant")
+            {
+                listOrdering.Add("TitrePassifDirigeant");
+            }
+            else if (unTri.TitlePassifDirigeantOrder == "TitlePassifDirigeantDesc")
+            {
+                listOrdering.Add("TitrePassifDirigeant desc");
+            }
+
+            if (unTri.PassifDirigeantOrder == "PassifDirigeant")
+            {
+                listOrdering.Add("PassifDirigeant");
+            }
+            else if (unTri.PassifDirigeantOrder == "PassifDirigeantDesc")
+            {
+                listOrdering.Add("PassifDirigeant desc");
+            }
+
+            if (unTri.NameEmpireOrder == "NameEmpire")
+            {
+                listOrdering.Add("NomEmpire");
+            }
+            else if (unTri.NameEmpireOrder == "NameEmpireDesc")
+            {
+                listOrdering.Add("NomEmpire desc");
+            }
+
+            if (unTri.TitlePassifEmpireOrder == "TitlePassifEmpire")
+            {
+                listOrdering.Add("TitrePassifEmpire");
+            }
+            else if (unTri.TitlePassifEmpireOrder == "TitlePassifEmpireDesc")
+            {
+                listOrdering.Add("TitrePassifEmpire desc");
+            }
+
+            if (unTri.PassifEmpireOrder == "PassifEmpire")
+            {
+                listOrdering.Add("PassifEmpire");
+            }
+            else if (unTri.PassifEmpireOrder == "PassifEmpireDesc")
+            {
+                listOrdering.Add("PassifEmpire desc");
+            }
+
+            if (unTri.NameQuartierOrder == "NameQuartier")
+            {
+                listOrdering.Add("NomQuartierEmpire");
+            }
+            else if (unTri.NameQuartierOrder == "NameQuartierDesc")
+            {
+                listOrdering.Add("NomQuartierEmpire desc");
+            }
+
+            if (unTri.QuartierOrder == "Quartier")
+            {
+                listOrdering.Add("QuartierEmpire");
+            }
+            else if (unTri.QuartierOrder == "QuartierDesc")
+            {
+                listOrdering.Add("QuartierEmpire desc");
+            }
+
+            if (unTri.NameBatimentOrder == "NameBatiment")
+            {
+                listOrdering.Add("NomBatimentEmpire");
+            }
+            else if (unTri.NameBatimentOrder == "NameBatimentDesc")
+            {
+                listOrdering.Add("NomBatimentEmpire desc");
+            }
+
+            if (unTri.BatimentOrder == "Batiment")
+            {
+                listOrdering.Add("BatimentEmpire");
+            }
+            else if (unTri.BatimentOrder == "BatimentDesc")
+            {
+                listOrdering.Add("BatimentEmpire desc");
+            }
+
+            if (unTri.NameAmenagementOrder == "NameAmenagement")
+            {
+                listOrdering.Add("NomAmenagementEmpire");
+            }
+            else if (unTri.NameAmenagementOrder == "NameAmenagementDesc")
+            {
+                listOrdering.Add("NomAmenagementEmpire desc");
+            }
+
+            if (unTri.AmenagementOrder == "Amenagement")
+            {
+                listOrdering.Add("AmenagementEmpire");
+            }
+            else if (unTri.AmenagementOrder == "AmenagementDesc")
+            {
+                listOrdering.Add("AmenagementEmpire desc");
+            }
+
+            return string.Join(',', listOrdering);
+        }
+
+      
     }
 }
