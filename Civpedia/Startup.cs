cd177736cabb9d4ab409f4558328a97722365530
Axel Vidoni <vidoni.axel@gmail.com>
2022-01-29 17:24:41 +0100

    ajout variable de session

--- Civpedia/Startup.cs
+++ Civpedia/Startup.cs
@@ -1,14 +1,10 @@
 using Civpedia.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Civpedia
 {
@@ -26,6 +22,13 @@ namespace Civpedia
         {
             services.AddControllersWithViews();
             services.AddTransient<CivpediaModel>();
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.MaxValue; //TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +50,8 @@ namespace Civpedia
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
