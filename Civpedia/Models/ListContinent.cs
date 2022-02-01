9b7c9ba1db4bc1233da2d588c845007bad882c1a
Axel Vidoni <vidoni.axel@gmail.com>
2022-02-01 18:45:54 +0100

    ajout tri

---
+++ Civpedia/Models/ListContinent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Civpedia.Models
+{
+    public static class ListContinent
+    {
+        public static List<string> ListEurope = new List<string>
+        {
+            "France",
+            "Angleterre",
+            "Italie",
+            "Espagne",
+            "Portugal",
+            "Suisse",
+            "Belgique"
+        };
+        public static List<string> ListAmerique = new List<string>
+        {
+            "Chili",
+            "Argentine",
+            "Bresil"
+        };
+        public static List<string> ListAsie = new List<string>
+        {
+            "Chine",
+            "Japon",
+        };
+        public static List<string> ListAfrique = new List<string>
+        {
+            "Cameroun"
+        };
+    }
+}
