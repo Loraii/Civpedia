3be2488bfd813820eb3de74295a8120ae31a4bb4
Axel Vidoni <vidoni.axel@gmail.com>
2022-02-02 09:20:42 +0100

    delete using inutile

--- Civpedia/Models/Classe/TriEntity.cs
+++ Civpedia/Models/Classe/TriEntity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Civpedia.Models
+﻿namespace Civpedia.Models
 {
     public class TriEntity
     {
